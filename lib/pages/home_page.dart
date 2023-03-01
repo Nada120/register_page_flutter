a654e2a0cff94f53dd8768e21f92e72b69fd8947
nada tarek <74355967+Nada120@users.noreply.github.com>
2023-03-01 21:12:19 +0200

    change color from purple to white

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
              style: TextStyle(
               fontSize: 50,
               fontWeight: FontWeight.w700,
-              color: Colors.purple,
+              color: Colors.white,
              ),
           ),
         ],
